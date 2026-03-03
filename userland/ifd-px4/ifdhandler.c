01f8b7104bdbf764faccf086ce0ce65c69137b77
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-03-03 22:44:53 +0900

    関数名変更

--- userland/ifd-px4/ifdhandler.c
+++ userland/ifd-px4/ifdhandler.c
@@ -54,15 +54,15 @@ static struct reader_context *get_reader(DWORD Lun)
 }
 
 /*
- * IFDHCreateChannelByName
+ * IFDHCreateChannel
  * Opens a communication channel to the device
  */
-RESPONSECODE IFDHCreateChannelByName(DWORD Lun, LPSTR DeviceName)
+RESPONSECODE IFDHCreateChannel(DWORD Lun, LPSTR DeviceName)
 {
 	struct reader_context *ctx;
 	int idx = get_reader_index(Lun);
 
-	Log1(PCSC_LOG_INFO, "IFDHCreateChannelByName");
+	Log1(PCSC_LOG_INFO, "IFDHCreateChannel");
 
 	if (idx < 0 || idx >= MAX_READERS) {
 		Log1(PCSC_LOG_ERROR, "Invalid LUN");
