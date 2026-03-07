1709e97de4a38824b60aca29d48ddf76ca00cd81
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-03-07 22:18:51 +0900

    型修正

--- include/px4_card_ioctl.h
+++ include/px4_card_ioctl.h
@@ -27,7 +27,7 @@
 /* ATR (Answer To Reset) structure - max 33 bytes per ISO/IEC 7816-3 */
 struct px4_card_atr {
 	unsigned char data[33];
-	unsigned int length;
+	unsigned char length;
 };
 
 /* Baudrate values */
